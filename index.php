24e505cb3ae99cbc89a73e1791fdfe2247572c7f
Fatokitemi01 <76545940+Fatokitemi01@users.noreply.github.com>
2021-04-21 05:36:04 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Upload files</title>
+
+</head>
+<body>
+<form action ="upload.php" method ="POST" >
+<input type ="file" name ="file">
+<br>
+<button type ="submit"name ="submit">DELETE</button>
+
+</form>
+
+</body>
+
+
+
+</html>
